2ca29bcbd6328ca3bc9097ac63947d4c847bc5aa
Yingzheng Ma <yma39@ford.com>
2019-06-18 15:57:15 -0700

    added root query type to get the user with a given id; WIP: implement resovle method

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -15,3 +15,17 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt }
   }
 });
+
+// root query - jump and land on a specific node of the graph
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString }}, // if given a user id, it should returns a UserType user.
+      // goes to the data graph and find the one we are looking for. e.g. a UserType user with id 23.
+      resolve(parentValue, args) {
+      }
+    }
+  }
+})
