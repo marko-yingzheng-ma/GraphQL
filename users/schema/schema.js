269c637623fb68556daf55d5057d68cf4e342f47
Yingzheng Ma <yma39@ford.com>
2019-06-18 16:29:24 -0700

    utilized schema

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -2,8 +2,15 @@ const graphql = require('graphql');
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema   // take a root query and returns a graphQL schema instance
 } = graphql;
+
+// dummy user data
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Sam', age: 21 }
+];
 
 // User schema
 const UserType = new GraphQLObjectType({
@@ -25,7 +32,12 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString }}, // if given a user id, it should returns a UserType user.
       // goes to the data graph and find the one we are looking for. e.g. a UserType user with id 23.
       resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
       }
     }
   }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
--- users/server.js
+++ users/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // set a expressGraphQL middleware to listen on GraphQL Request
 app.use('/graphql', expressGraphQL({
+    schema: schema,
     // this is a dev tool that allows us to make query against dev server
     graphiql: true
 }))
