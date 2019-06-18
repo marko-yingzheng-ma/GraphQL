9711c8d8bdc0a5513d1be701cce3b07fcb6d51bd
Marko Ma <yma004@drury.edu>
2019-06-17 18:54:26 -0700

    set up express-graphql middleware; WIP: add schema

--- users/server.js
+++ users/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+// set a expressGraphQL middleware to listen on GraphQL Request
+app.use('/graphql', expressGraphQL({
+    // this is a dev tool that allows us to make query against dev server
+    graphiql: true
+}))
 
 app.listen(4000, () => {
     console.log('server running on port 4000');
